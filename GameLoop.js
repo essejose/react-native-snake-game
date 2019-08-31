998ee463d13e63d129d311fcc83af5be8b86f564
Jose <ze_pk2@hotmail.com>
2019-08-31 15:54:47 -0300

    logica de game over

--- GameLoop.js
+++ GameLoop.js
@@ -7,8 +7,22 @@ const GameLoop = (entities, { touches, dispatch, events }) =>{
     head.nextMove -=1;
     if(head.nextMove === 0){
         head.nextMove = head.updateFrequency;
-        head.position[0] += head.xspeed;
-        head.position[1] += head.yspeed;
+
+
+        if( 
+            head.position[0]  + head.xspeed < 0 ||
+            head.position[0]  + head.xspeed >=  Constants.GRID_SIZE ||
+            head.position[1]  + head.yspeed < 0 ||
+            head.position[1]  + head.yspeed >= Constants.GRID_SIZE
+        ){
+            // Game over :(
+        } else{
+            
+            head.position[0] += head.xspeed;
+            head.position[1] += head.yspeed;
+    
+        }
+
     }
 
     return entities
